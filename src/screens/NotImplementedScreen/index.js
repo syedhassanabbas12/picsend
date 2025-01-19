78324cf748c7d991105b2b47abea18a8c55994df
= <hassanabbassayed@gmail.com>
2025-01-19 12:15:40 +0800

    minor change

--- src/screens/NotImplementedScreen/index.js
+++ src/screens/NotImplementedScreen/index.js
@@ -5,7 +5,7 @@ import styles from "./styles";
 const NotImplementedScreen = () => {
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Not Implemented Screen</Text>
+      <Text style={styles.title}>Not Implemented Screen(s)</Text>
     </View>
   );
 };
